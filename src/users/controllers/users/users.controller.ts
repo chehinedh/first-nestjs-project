553c4df44f9c23034ed300affef848f51d4a8508
chehinedh <chehine.dhemaied@esprit.tn>
2023-12-30 14:25:00 +0100

    🚧 Handling Routes

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUsers() {
+    return { username: 'Chehine', email: 'chehine@gmail.com' };
+  }
+
+  @Get('posts')
+  getUsersPosts() {
+    return [
+      {
+        username: 'Chehine',
+        email: 'chehine@gmail.com',
+        posts: [
+          {
+            id: 1,
+            title: 'Post 1',
+          },
+          {
+            id: 2,
+            title: 'Post 2',
+          },
+        ],
+      },
+    ];
+  }
+
+  @Get('posts/comments')
+  getUsersPostsComments() {
+    return [
+      {
+        id: 1,
+        title: 'Post 1',
+        comments: [],
+      },
+    ];
+  }
+}
