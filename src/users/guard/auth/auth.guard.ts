b22e22a46ed1331fe7d21753c595c96553ac2632
chehinedh <chehine.dhemaied@esprit.tn>
2024-01-01 18:46:32 +0100

    🔐 Guards

--- src/users/guard/auth/auth.guard.ts
+++ src/users/guard/auth/auth.guard.ts
@@ -7,6 +7,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const request = context.switchToHttp().getRequest() as Request;
     return false;
   }
